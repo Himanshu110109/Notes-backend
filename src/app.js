a02464c09cc41b5bce9d92ab9be48ae255025380
Himanshu <158292948+Himanshu110109@users.noreply.github.com>
2025-01-11 09:33:24 +0530

    Update app.js
    ...

--- src/app.js
+++ src/app.js
@@ -2,16 +2,17 @@ const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+require('dotenv').config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Connection URI
-const uri = "mongodb://localhost:27017";
+const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri);
 const dbName = "notesApp";
 
